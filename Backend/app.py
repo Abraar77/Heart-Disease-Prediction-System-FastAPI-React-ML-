9d64fc763b760f00363b6765758ac0be3dec3f60
Abraar <tr.abraar786@gmail.com>
2025-12-25 16:43:18 +0530

    Enable CORS for frontend

--- Backend/app.py
+++ Backend/app.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],  # later you can restrict this
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
