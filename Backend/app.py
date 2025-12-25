c7c7afd7da61527422252baf4173c1f27ef3e3cc
Abraar <tr.abraar786@gmail.com>
2025-12-25 20:24:36 +0530

    again cors

--- Backend/app.py
+++ Backend/app.py
@@ -10,6 +10,7 @@ app.add_middleware(
     CORSMiddleware,
      allow_origins=[
         "https://sheen-prediction-bbcb.onrender.com/"
+        "https://sheen-prediction-bbcb.onrender.com"
     ],
     allow_credentials=False,
     allow_methods=["*"],
